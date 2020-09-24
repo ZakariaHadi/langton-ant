2e07053258887da25a4972af0ebc43fdd9ab46d3
zakaria hadi <z.hadi@sanadtech.com>
2020-09-24 10:22:40 +0200

    Add init function

--- main.c
+++ main.c
@@ -3,18 +3,25 @@
 #include "initialisation_fenetre.h"
 
 
+void init_params(IlisiFourmi* Jeu)
+{
+    Jeu->iteration_precise=0;
+        Jeu->play=0;
+        Jeu->fourmi_test=0;
+        Jeu->iter=0;
+        Jeu->nbr_fourmi=0;
+        Jeu->vitesse=100;
+        Jeu->fourmis=NULL;
+        return void;
+}
+
+
     int main(int argc, char** argv)
 {
         gtk_init(&argc, &argv);
 
         IlisiFourmi Jeu;
-        Jeu.iteration_precise=0;
-        Jeu.play=0;
-        Jeu.fourmi_test=0;
-        Jeu.iter=0;
-        Jeu.nbr_fourmi=0;
-        Jeu.vitesse=100;
-        Jeu.fourmis=NULL;
+        init_params(&Jeu);
         init_interface(&Jeu);
 
 
